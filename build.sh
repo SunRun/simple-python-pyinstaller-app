d95c988a2b04fefdf3b209681cf64faa7e5a6d41
Sai Kanuparthy <sai.kanuparthy@sunrun.com>
2018-11-15 13:22:57 -0800

    Modify 'Build' stage with build.sh file

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # Build 
 echo 'Building..'
-sh 'python -m py_compile sources/add2vals.py sources/calc.py'                 
+sh "python -m py_compile sources/add2vals.py sources/calc.py"                
